9bf1d84de83bfd0da76a652c6d1f95dd6a0e4b89
ARKANYOTA <38374187+ARKANYOTA@users.noreply.github.com>
2020-10-31 00:13:00 +0100

    CSS updates annimation2

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function darklight() {
     document.body.classList.toggle("dark-mode");
 }
 
+
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     darklight()
 };
 
+
+function menushow() {
+    var menuelement = ["pro", "par", "skil", "soc"];
+    menuelement.forEach(function(item, index){
+        var x = document.getElementById(item);
+        if (x.style.display === "none") { x.style.display = "block"; } 
+        else { x.style.display = "none";}
+    });
+    
+}
